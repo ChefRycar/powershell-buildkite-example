6d4822461c1d430155cd49fe8ab2ebf71f61cd4e
Deborah Barnard <starfallprojects@gmail.com>
2021-04-07 15:38:19 +0100

    remove the pretty

--- script.ps1
+++ script.ps1
@@ -11,7 +11,7 @@ $myenv = Get-Variable
 Write-Output "Build job environment: `n  $myenv"
 
 
-Write-Output "+++ :hammer: Example tests"
+Write-Output "Example tests"
 
 Write-Output "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
 
